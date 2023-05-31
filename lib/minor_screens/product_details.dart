fb447e6ce11a4f9bd0b893eb3d733e2f64ea27ee
maisylattanavong <tialattanavong@gmail.com>
2023-05-31 10:52:15 +0700

    product detail

--- lib/minor_screens/product_details.dart
+++ lib/minor_screens/product_details.dart
@@ -12,17 +12,117 @@ class _ProductDetailsScreenState extends State<ProductDetailsScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Column(children: [
-        SizedBox(
-          height: MediaQuery.of(context).size.height * 0.45,
-          child: Swiper(
-              itemBuilder: (context, index) {
-                return const Image(
-                    image: AssetImage('images/beauty/beauty0.jpg'));
-              },
-              itemCount: 1),
-        )
-      ]),
+      body: Column(
+        children: [
+          SizedBox(
+            height: MediaQuery.of(context).size.height * 0.45,
+            child: Swiper(
+                pagination:
+                    const SwiperPagination(builder: SwiperPagination.fraction),
+                itemBuilder: (context, index) {
+                  return const Image(
+                      image: AssetImage('images/beauty/beauty0.jpg'));
+                },
+                itemCount: 1),
+          ),
+          Text(
+            'pro namessss',
+            style: TextStyle(
+                fontSize: 20,
+                color: Colors.grey.shade600,
+                fontWeight: FontWeight.w600),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [
+              const Row(
+                children: [
+                  Text(
+                    'USD',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.red,
+                        fontWeight: FontWeight.w600),
+                  ),
+                  Text(
+                    '190',
+                    style: TextStyle(
+                        fontSize: 20,
+                        color: Colors.red,
+                        fontWeight: FontWeight.w600),
+                  ),
+                ],
+              ),
+              IconButton(
+                onPressed: () {},
+                icon: const Icon(
+                  Icons.favorite_border_outlined,
+                  color: Colors.red,
+                  size: 30,
+                ),
+              ),
+            ],
+          ),
+          const Text(
+            '99 pieces available in stock',
+            style: TextStyle(fontSize: 16, color: Colors.blueGrey),
+          ),
+          const ProDetailsHeader(
+            label: '   Item Description   ',
+          ),
+          Text(
+            'pro descsssss',
+            style: TextStyle(
+                fontSize: 20,
+                fontWeight: FontWeight.w600,
+                color: Colors.blueGrey.shade800),
+          ),
+          const ProDetailsHeader(
+            label: '   Similar Items   ',
+          ),
+        ],
+      ),
+    );
+  }
+}
+
+class ProDetailsHeader extends StatelessWidget {
+  final String label;
+  const ProDetailsHeader({super.key, required this.label});
+
+  @override
+  Widget build(BuildContext context) {
+    return SizedBox(
+      height: 60,
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          SizedBox(
+            height: 40,
+            width: 50,
+            child: Divider(
+              color: Colors.yellow.shade900,
+              thickness: 1,
+            ),
+          ),
+          Text(
+            label,
+            style: TextStyle(
+              color: Colors.yellow.shade900,
+              fontSize: 24,
+              fontWeight: FontWeight.w600,
+            ),
+          ),
+          SizedBox(
+            height: 40,
+            width: 50,
+            child: Divider(
+              color: Colors.yellow.shade900,
+              thickness: 1,
+            ),
+          )
+        ],
+      ),
     );
   }
 }
